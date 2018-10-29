d21996379c7cf67de589473370ecf948a99d225a
James Cameron <quozl@laptop.org>
2018-10-29 11:12:42 +1100

    Remove unused import

--- reflectwindow.py
+++ reflectwindow.py
@@ -19,7 +19,6 @@ from gettext import gettext as _
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
-from gi.repository import GConf
 from gi.repository import GdkPixbuf
 from gi.repository import Pango
 
