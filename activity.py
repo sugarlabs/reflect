1ecf775c86422a7d9ec5ae248580ec72e43f6da6
abhay malik <eshanmalik150216@gmail.com>
2020-01-21 23:36:03 +0800

    Updated Telepathy

--- activity.py
+++ activity.py
@@ -42,7 +42,7 @@ from sugar3.graphics import style
 from sugar3 import profile
 from sugar3.datastore import datastore
 
-import telepathy
+from gi.repository import TelepathyGLib
 import dbus
 from dbus.service import signal
 from dbus.gobject_service import ExportedGObject
