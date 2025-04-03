9315b92efad1adb12c9742eee8221b08ca35891d
Devmoni <devmoni97@gmail.com>
2025-04-03 17:52:01 +0530

    Fix PyGI warnings and deprecated GObject flag usage
    - Added gi.require_version() for Vte, Wnck, SugarExt, and Gio to fix PyGIWarnings.
    - Replaced GConf with GSettings (Gio) for settings management.
    - Updated deprecated GObject.PARAM_READABLE to GObject.ParamFlags.READABLE.

--- graphics.py
+++ graphics.py
@@ -13,6 +13,8 @@
 from gettext import gettext as _
 
 import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
 gi.require_version('WebKit2', '4.0')
 
 from gi.repository import Gdk
--- reflectwindow.py
+++ reflectwindow.py
@@ -16,6 +16,9 @@ import json
 from random import uniform
 from gettext import gettext as _
 
+import gi
+gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
